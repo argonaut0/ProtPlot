45ce000cbb6ef297d971c12640518d88d3833029
Jason Hsu <jasonhsu2013@gmail.com>
2022-01-11 23:52:45 -0800

    Port over scale calculator

---
+++ src/routes/_graph.svelte
@@ -0,0 +1,8 @@
+<script>
+    import DataContainer from '@snlab/florence-datacontainer';
+
+    export const testData = {
+        protein1: [3.4, 5.4, 8.2, 8.9, -2.9, -2.2, -1.8, 8.7,-3.2,0.3,-8.7,-4.4,1.1,-1.3,7.7,-7.9,-3.4,-8.1,-6.8,-5.5,4.5,7.9,],
+        protein2: [1,4,5,6,2],
+    };
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import { testData } from "../../static/scale";
+</script>
+
+
+<h1>ProtPlot v0.1</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
